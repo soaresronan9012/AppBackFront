b3385110814606470c0b150a83edc60cd941d85e
Ronan Soares <soaresronansilva@gmail.com>
2024-09-30 21:59:37 -0300

    configurando VIEWCODE

--- AppBackFront/SceneDelegate.swift
+++ AppBackFront/SceneDelegate.swift
@@ -13,10 +13,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow ( windowScene: windowScene) // criou a window
+        let vc : ViewController = ViewController() // instanciou a vc root
+        window.rootViewController = vc // atribuiu como root
+        window.makeKeyAndVisible() // habilita criacao
+        self.window = window // atribui essa view como a view padrao
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
